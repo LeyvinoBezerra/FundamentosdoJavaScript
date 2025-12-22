665aaa8fe214be2f41973e10222d0cccfc51a76e
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2025-12-22 15:14:53 -0300

    Atualiza var.js

--- FirstSteps/var.js
+++ FirstSteps/var.js
@@ -1,3 +1,14 @@
+// "let" define uma variável que pode ser inicializada na criação 
+// ou posteriormente apenas com a atribuição de um valor.
+
+let idade; // variável declarada sem valor inicial
+idade = 25; // atribuição feita depois
+
+let nome = "Leyvino"; // variável já inicializada na criação
+
+
+
+
 //let a;
 //let b;
 //a = 10;
