3c6e4bc0441f21d74e0798cc7c2edebdff4bdb47
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2025-12-30 23:46:19 -0300

    Final da noite no meu HomeOffice Na Paz, Musica belchior: como nossos pais, filmes: bons Companheiro Niro

--- SelectionStructure/if.js
+++ SelectionStructure/if.js
@@ -3,8 +3,13 @@
 const temp = 26;
 
 if (temp > 25) {
-    console.log("Está calor!"); 
+    // A indentação (espaço/tabulação antes do código) é importante
+    // porque deixa claro quais instruções pertencem ao bloco do if.
+    // Aqui, as linhas dentro das chaves estão indentadas para mostrar
+    // que só serão executadas se a condição (temp > 25) for verdadeira.
+    console.log("Está calor!");  
     console.log("Temperatura é " + temp);
 }
 
-console.log("Fim");
+console.log("Fim"); 
+// Fora do bloco, sem indentação extra, indicando que será executado sempre.
